efff1b0505cd1f4fa2c4c41a5f430d6ec47da0b5
Optofizik <optofizik@gmail.com>
2017-03-30 22:09:39 +0300

    Finished example

--- ReactMVC/Controllers/HomeController.cs
+++ ReactMVC/Controllers/HomeController.cs
@@ -3,14 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ReactMVC.Models;
 
 namespace ReactMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<Comment> comments;
+
+        public HomeController()
+        {
+            comments = new List<Comment>()
+            {
+                new Comment
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new Comment
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new Comment
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                }
+            };
+        }
+
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Comments()
+        {
+            return Json(comments, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(Comment model)
+        {
+            model.Id = comments.Max(c => c.Id) + 1;
+            comments.Add(model);
+            return Content("Success :)");
         }
     }
 }
--- ReactMVC/Scripts/Tutorial.jsx
+++ ReactMVC/Scripts/Tutorial.jsx
@@ -44,28 +44,94 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+
+    getInitialState: function () {
+        return { author: "", text: "" };
+    },
+
+    handleAuthorChange: function (e) {
+        this.setState({ author: e.target.value });
+    },
+
+    handleTextChange: function (e) {
+        this.setState({ text: e.target.value });
+    },
+
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+
+        if (!author || !text) {
+            return false;
+        }
+        this.props.onCommentSubmit({ Author: author, Text: text });
+        this.setState({ author: "", text: "" });
+    },
+
     render: function () {
         return (
-            <div className="commentForm">
-                Hello, world! I am a CommentForm.
-            </div>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input type="text" name="Author"
+                       placeholder="Your name"
+                       value={this.state.author}
+                       onChange={this.handleAuthorChange} />
+                <input type="text" name="Text"
+                       placeholder="Say something..."
+                       value={this.state.text}
+                       onChange={this.handleTextChange} />
+                <input type="submit" value="Post" />
+            </form>
         );
     }
 });
 
 var CommentBox = React.createClass({
+
+    loadCommentsFromServer: function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        }.bind(this);
+        xhr.send();
+    },
+
+    handleCommentSubmit: function (comment) {
+
+        var data = new FormData();
+        data.append("Author", comment.Author);
+        data.append("Text", comment.Text);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("post", this.props.submitUrl, true);
+        xhr.onload = function () {
+            this.loadCommentsFromServer();
+        }.bind(this);
+        xhr.send(data);
+    },
+
+    getInitialState: function () {
+        return { data: [] };
+    },
+
+    componentDidMount: function () {
+        this.loadCommentsFromServer();
+    },
+
     render: function () {
         return (
         <div className="commentBox">
             Hello, World! I am a Comment Box!
             <h1>Comments</h1>
-            <CommentList data={this.props.data} />
-            <CommentForm />
+            <CommentList data={this.state.data} />
+            <CommentForm onCommentSubmit={this.handleCommentSubmit} />
         </div>);
     }
 });
 
 ReactDOM.render(
-    <CommentBox data={data} />,
+    <CommentBox url="/comments" submitUrl="/comments/new" pollInterval={2000} />,
     document.getElementById("content")
 );
